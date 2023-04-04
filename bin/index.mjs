66b6b2bc269b6ef654b1618a542b105c4485698f
JoshAaronLevy <joshaaronlevy@gmail.com>
2023-04-04 10:26:33 -0600

    Initial push

---
+++ bin/index.mjs
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+const program = new Command();
+import prompts from "../lib/prompts.mjs";
+
+program
+  .description("Example: dirtective \"./\"")
+  .version("1.0.0", "-v, --version")
+  .action(async (message, command) => {
+    console.log("message: ", message);
+    console.log("command: ", command);
+    if (message.branch === true || message.b === true) {
+      command = message;
+      await prompts(command);
+      return;
+    } else if (!command || command === undefined) {
+      command = message;
+      await prompts(command);
+      return;
+    } else {
+      await prompts(command);
+      return;
+    }
+  });
+
+program.parse(process.argv);
